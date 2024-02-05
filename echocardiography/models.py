287ddfc138afd416482580878ebac77aa9f4297f
AngeloLasala <lasala.angelo96@gmail.com>
2024-02-05 16:26:40 +0100

    add simple resnet for regression

--- echocardiography/models.py
+++ echocardiography/models.py
@@ -3,8 +3,25 @@ Structure of the models used in automatic detection of keypoits in PLAX echocard
 """
 import torch
 import torchvision
+from torchvision import models
 from pathlib import Path
 from torchsummary import summary 
+
+class ResNet50Regression(torch.nn.Module):
+    def __init__(self, input_channels=3, num_labels=6):
+        super(ResNet50Regression, self).__init__()
+        # Load the pre-trained ResNet50 model
+        resnet50 = models.resnet50(weights='DEFAULT')
+         
+        # Add a new regression layer
+        self.resnet50 = resnet50    
+        self.resnet50.fc = torch.nn.Sequential(torch.nn.Linear(resnet50.fc.in_features, num_labels),
+                                                    torch.nn.Sigmoid())
+
+    def forward(self, x):
+        x = self.resnet50(x)
+        x = x.view(x.size(0), -1)
+        return x
 
 
 class PlaxModel(torch.nn.Module):
@@ -23,10 +40,18 @@ class PlaxModel(torch.nn.Module):
     def forward(self, x):
         return torch.sigmoid(self.model(x)['out'])
 
-model = PlaxModel()
-# print(summary(model, (3, 224, 224)))
-sequential_block = model.model.backbone  # Replace with the specific Sequential block you want to inspect
-print(sequential_block)
+
+### SimpleRegression model
+model = ResNet50Regression()
+x = torch.randn(1, 3, 256, 256)
+print(model(x))
+
+
+### PLAX model
+# model = PlaxModel()
+# # print(summary(model, (3, 224, 224)))
+# sequential_block = model.model.backbone  # Replace with the specific Sequential block you want to inspect
+# print(sequential_block)
 
 # Iterate through the layers inside the Sequential block
 # for layer_idx, layer in enumerate(sequential_block.children()):
