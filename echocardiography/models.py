ae822c09d5d7cfc362ee6c63c6661e2a7c70a237
AngeloLasala <lasala.angelo96@gmail.com>
2024-02-12 14:30:42 +0100

    update: added U-Net model for semantic segmentation

--- echocardiography/models.py
+++ echocardiography/models.py
@@ -2,12 +2,14 @@
 Structure of the models used in automatic detection of keypoits in PLAX echocardiography.
 """
 import torch
+import torch.nn as nn
 import torchvision
 from torchvision import models
+import torch.nn.functional as F
 from pathlib import Path
 from torchsummary import summary 
 
-class ResNet50Regression(torch.nn.Module):
+class ResNet50Regression(nn.Module):
     def __init__(self, 
                 input_channels=3, 
                 num_labels=12):
@@ -17,8 +19,8 @@ class ResNet50Regression(torch.nn.Module):
          
         # Add a new regression layer
         self.resnet50 = resnet50    
-        self.resnet50.fc = torch.nn.Sequential(torch.nn.Linear(resnet50.fc.in_features, num_labels),
-                                                    torch.nn.Sigmoid())
+        self.resnet50.fc = nn.Sequential(nn.Linear(resnet50.fc.in_features, num_labels),
+                                                    nn.Sigmoid())
 
     def forward(self, x):
         x = self.resnet50(x)
@@ -41,6 +43,107 @@ class PlaxModel(torch.nn.Module):
     def forward(self, x):
         return torch.sigmoid(self.model(x)['out'])
 
+class UNet(nn.Module):
+    def __init__(self, in_channels=3, num_classes=1):
+        super(UNet, self).__init__()
+
+        # Contracting Path
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, padding='same')
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding='same')
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding='same')
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding='same')
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding='same')
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, padding='same')
+        self.pool3 = nn.MaxPool2d(2)
+
+        self.conv7 = nn.Conv2d(256, 512, kernel_size=3, padding='same')
+        self.conv8 = nn.Conv2d(512, 512, kernel_size=3, padding='same')
+        self.pool4 = nn.MaxPool2d(2)
+
+        self.conv9 = nn.Conv2d(512, 1024, kernel_size=3, padding='same')
+        self.conv10 = nn.Conv2d(1024, 1024, kernel_size=3, padding='same')
+
+        # Expanding Path
+        self.up1 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.conv11 = nn.Conv2d(1024, 512, kernel_size=2, padding='same')
+        self.conv12 = nn.Conv2d(1024, 512, kernel_size=3, padding='same')
+        self.conv13 = nn.Conv2d(512, 512, kernel_size=3, padding='same')
+
+        self.up2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.conv14 = nn.Conv2d(512, 256, kernel_size=2, padding='same')
+        self.conv15 = nn.Conv2d(512, 256, kernel_size=3, padding='same')
+        self.conv16 = nn.Conv2d(256, 256, kernel_size=3, padding='same')
+
+        self.up3 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.conv17 = nn.Conv2d(256, 128, kernel_size=2, padding='same')
+        self.conv18 = nn.Conv2d(256, 128, kernel_size=3, padding='same')
+        self.conv19 = nn.Conv2d(128, 128, kernel_size=3, padding='same')
+
+        self.up4 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.conv20 = nn.Conv2d(128, 64, kernel_size=2, padding='same')
+        self.conv21 = nn.Conv2d(128, 64, kernel_size=3, padding='same')
+        self.conv22 = nn.Conv2d(64, 64, kernel_size=3, padding='same')
+
+        # Output Layer
+        self.conv23 = nn.Conv2d(64, num_classes, kernel_size=3, padding='same')
+        # self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # Encoder
+        conv1 = F.relu(self.conv1(x))
+        conv2 = F.relu(self.conv2(conv1))
+        pool1 = self.pool1(conv2)
+
+        conv3 = F.relu(self.conv3(pool1))
+        conv4 = F.relu(self.conv4(conv3))
+        pool2 = self.pool2(conv4)
+
+        conv5 = F.relu(self.conv5(pool2))
+        conv6 = F.relu(self.conv6(conv5))
+        pool3 = self.pool3(conv6)
+
+        conv7 = F.relu(self.conv7(pool3))
+        conv8 = F.relu(self.conv8(conv7))
+        pool4 = self.pool4(conv8)
+
+        conv9 = F.relu(self.conv9(pool4))
+        conv10 = F.relu(self.conv10(conv9))
+
+        # Decoder
+        up1 = self.up1(conv10)
+        conv11 = F.relu(self.conv11(up1))
+        cat1 = torch.cat([conv11, conv8], dim=1)
+        conv12 = F.relu(self.conv12(cat1))
+        conv13 = F.relu(self.conv13(conv12))
+
+        up2 = self.up2(conv13)
+        conv14 = F.relu(self.conv14(up2))
+        cat2 = torch.cat([conv14, conv6], dim=1)
+        conv15 = F.relu(self.conv15(cat2))
+        conv16 = F.relu(self.conv16(conv15))
+
+        up3 = self.up3(conv16)
+        conv17 = F.relu(self.conv17(up3))
+        cat3 = torch.cat([conv17, conv4], dim=1)
+        conv18 = F.relu(self.conv18(cat3))
+        conv19 = F.relu(self.conv19(conv18))
+
+        up4 = self.up4(conv19)
+        conv20 = F.relu(self.conv20(up4))
+        cat4 = torch.cat([conv20, conv2], dim=1)
+        conv21 = F.relu(self.conv21(cat4))
+        conv22 = F.relu(self.conv22(conv21))
+
+        # Output Layer
+        conv23 = F.relu(self.conv23(conv22))
+        output = self.sigmoid(conv23)
+
+        return output
+
 if __name__ == '__main__':
     ## SimpleRegression model
     model = ResNet50Regression()
@@ -61,8 +164,20 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     plt.figure()
     plt.imshow(pred[0, 0, :, :])
+    # plt.show()
+
+    ## UNet model
+    model = UNet()
+    model.train()
+    x = torch.randn(2, 3, 256, 256)
+    print(model(x).shape)
+    pred = model(x).detach().numpy()
+
+    plt.figure()
+    plt.imshow(pred[0, 0, :, :])
     plt.show()
 
+    
 
     # Iterate through the layers inside the Sequential blockKC
     # for layer_idx, layer in enumerate(sequential_block.children()):
