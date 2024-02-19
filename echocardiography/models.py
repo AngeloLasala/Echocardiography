3e9e0fb1a61c4076968360ed1521ca3e81e0a119
AngeloLasala <lasala.angelo96@gmail.com>
2024-02-19 12:11:05 +0100

    added unet model with upsampling and batch normalization

--- echocardiography/models.py
+++ echocardiography/models.py
@@ -44,156 +44,196 @@ class PlaxModel(torch.nn.Module):
     def forward(self, x):
         return torch.sigmoid(self.model(x)['out'])
 
-# class UNet(nn.Module):
-#     def __init__(self, in_channels=3, num_classes=1):
-#         super(UNet, self).__init__()
+class UNet_up(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        
+        # Encoder
+        self.e11 = nn.Conv2d(3, 64, kernel_size=3, padding=1) # output: 570x570x64
+        self.bn11 = nn.BatchNorm2d(64)
+        self.e12 = nn.Conv2d(64, 64, kernel_size=3, padding=1) # output: 568x568x64
+        self.bn12 = nn.BatchNorm2d(64)
 
-#         # Contracting Path
-#         self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, padding='same')
-#         self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding='same')
-#         self.pool1 = nn.MaxPool2d(2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 284x284x64
 
-#         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding='same')
-#         self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding='same')
-#         self.pool2 = nn.MaxPool2d(2)
+        # input: 284x284x64
+        self.e21 = nn.Conv2d(64, 128, kernel_size=3, padding=1) # output: 282x282x128
+        self.bn21 = nn.BatchNorm2d(128)
+        self.e22 = nn.Conv2d(128, 128, kernel_size=3, padding=1) # output: 280x280x128
+        self.bn22 = nn.BatchNorm2d(128)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 140x140x128
 
-#         self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding='same')
-#         self.conv6 = nn.Conv2d(256, 256, kernel_size=3, padding='same')
-#         self.pool3 = nn.MaxPool2d(2)
+        # input: 140x140x128
+        self.e31 = nn.Conv2d(128, 256, kernel_size=3, padding=1) # output: 138x138x256
+        self.bn31 = nn.BatchNorm2d(256)
+        self.e32 = nn.Conv2d(256, 256, kernel_size=3, padding=1) # output: 136x136x256
+        self.bn32 = nn.BatchNorm2d(256)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 68x68x256
 
-#         self.conv7 = nn.Conv2d(256, 512, kernel_size=3, padding='same')
-#         self.conv8 = nn.Conv2d(512, 512, kernel_size=3, padding='same')
-#         self.pool4 = nn.MaxPool2d(2)
+        # input: 68x68x256
+        self.e41 = nn.Conv2d(256, 512, kernel_size=3, padding=1) # output: 66x66x512
+        self.bn41 = nn.BatchNorm2d(512)
+        self.e42 = nn.Conv2d(512, 512, kernel_size=3, padding=1) # output: 64x64x512
+        self.bn42 = nn.BatchNorm2d(512)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 32x32x512
 
-#         self.conv9 = nn.Conv2d(512, 1024, kernel_size=3, padding='same')
-#         self.conv10 = nn.Conv2d(1024, 1024, kernel_size=3, padding='same')
+        # input: 32x32x512
+        self.e51 = nn.Conv2d(512, 1024, kernel_size=3, padding=1) # output: 30x30x1024
+        self.bn51 = nn.BatchNorm2d(1024)
+        self.e52 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1) # output: 28x28x1024
+        self.bn52 = nn.BatchNorm2d(1024)
 
-#         # Expanding Path
-#         self.up1 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
-#         self.conv11 = nn.Conv2d(1024, 512, kernel_size=2, padding='same')
-#         self.conv12 = nn.Conv2d(1024, 512, kernel_size=3, padding='same')
-#         self.conv13 = nn.Conv2d(512, 512, kernel_size=3, padding='same')
 
-#         self.up2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
-#         self.conv14 = nn.Conv2d(512, 256, kernel_size=2, padding='same')
-#         self.conv15 = nn.Conv2d(512, 256, kernel_size=3, padding='same')
-#         self.conv16 = nn.Conv2d(256, 256, kernel_size=3, padding='same')
+        # Decoder
+        self.upconv1 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv1_1 = nn.Conv2d(1024,512, kernel_size=1, stride=1)
 
-#         self.up3 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
-#         self.conv17 = nn.Conv2d(256, 128, kernel_size=2, padding='same')
-#         self.conv18 = nn.Conv2d(256, 128, kernel_size=3, padding='same')
-#         self.conv19 = nn.Conv2d(128, 128, kernel_size=3, padding='same')
+        self.d11 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
+        self.bn_d11 = nn.BatchNorm2d(512)
+        self.d12 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn_d12 = nn.BatchNorm2d(512)
 
-#         self.up4 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
-#         self.conv20 = nn.Conv2d(128, 64, kernel_size=2, padding='same')
-#         self.conv21 = nn.Conv2d(128, 64, kernel_size=3, padding='same')
-#         self.conv22 = nn.Conv2d(64, 64, kernel_size=3, padding='same')
+        self.upconv2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv1_2 = nn.Conv2d(512, 256, kernel_size=1, stride=1)
 
-#         # Output Layer
-#         self.conv23 = nn.Conv2d(64, num_classes, kernel_size=3, padding='same')
-#         self.sigmoid = nn.Sigmoid()
+        self.d21 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
+        self.bn_d21 = nn.BatchNorm2d(256)
+        self.d22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn_d22 = nn.BatchNorm2d(256)
 
-#     def forward(self, x):
-#         # Encoder
-#         conv1 = F.relu(self.conv1(x))
-#         conv2 = F.relu(self.conv2(conv1))
-#         pool1 = self.pool1(conv2)
+        self.upconv3 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv1_3 = nn.Conv2d(256, 128, kernel_size=1, stride=1)
 
-#         conv3 = F.relu(self.conv3(pool1))
-#         conv4 = F.relu(self.conv4(conv3))
-#         pool2 = self.pool2(conv4)
+        self.d31 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+        self.bn_d31 = nn.BatchNorm2d(128)
+        self.d32 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.bn_d32 = nn.BatchNorm2d(128)
 
-#         conv5 = F.relu(self.conv5(pool2))
-#         conv6 = F.relu(self.conv6(conv5))
-#         pool3 = self.pool3(conv6)
+        self.upconv4 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv1_4 = nn.Conv2d(128, 64, kernel_size=1, stride=1)
 
-#         conv7 = F.relu(self.conv7(pool3))
-#         conv8 = F.relu(self.conv8(conv7))
-#         pool4 = self.pool4(conv8)
+        self.d41 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
+        self.bn_d41 = nn.BatchNorm2d(64)
+        self.d42 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.bn_d42 = nn.BatchNorm2d(64)
 
-#         conv9 = F.relu(self.conv9(pool4))
-#         conv10 = F.relu(self.conv10(conv9))
+        # Output layer
+        self.outconv = nn.Conv2d(64, num_classes, kernel_size=1)
+        self.sigmoid = nn.Sigmoid()
 
-#         # Decoder
-#         up1 = self.up1(conv10)
-#         conv11 = F.relu(self.conv11(up1))
-#         cat1 = torch.cat([conv11, conv8], dim=1)
-#         conv12 = F.relu(self.conv12(cat1))
-#         conv13 = F.relu(self.conv13(conv12))
+    def forward(self, x):
+        # Encoder
+        xe11 = F.relu(self.bn11(self.e11(x)))
+        xe12 = F.relu(self.bn12(self.e12(xe11)))
+        xp1 = self.pool1(xe12)
 
-#         up2 = self.up2(conv13)
-#         conv14 = F.relu(self.conv14(up2))
-#         cat2 = torch.cat([conv14, conv6], dim=1)
-#         conv15 = F.relu(self.conv15(cat2))
-#         conv16 = F.relu(self.conv16(conv15))
+        xe21 = F.relu(self.bn21(self.e21(xp1)))
+        xe22 = F.relu(self.bn22(self.e22(xe21)))
+        xp2 = self.pool2(xe22)
 
-#         up3 = self.up3(conv16)
-#         conv17 = F.relu(self.conv17(up3))
-#         cat3 = torch.cat([conv17, conv4], dim=1)
-#         conv18 = F.relu(self.conv18(cat3))
-#         conv19 = F.relu(self.conv19(conv18))
+        xe31 = F.relu(self.bn31(self.e31(xp2)))
+        xe32 = F.relu(self.bn32(self.e32(xe31)))
+        xp3 = self.pool3(xe32)
 
-#         up4 = self.up4(conv19)
-#         conv20 = F.relu(self.conv20(up4))
-#         cat4 = torch.cat([conv20, conv2], dim=1)
-#         conv21 = F.relu(self.conv21(cat4))
-#         conv22 = F.relu(self.conv22(conv21))
+        xe41 = F.relu(self.bn41(self.e41(xp3)))
+        xe42 = F.relu(self.bn42(self.e42(xe41)))
+        xp4 = self.pool4(xe42)
 
-#         # Output Layer
-#         conv23 = F.relu(self.conv23(conv22))
-#         output = self.sigmoid(conv23)
+        xe51 = F.relu(self.bn51(self.e51(xp4)))
+        xe52 = F.relu(self.bn52(self.e52(xe51)))
+        
+        # Decoder
+        xu1 = self.conv1_1(self.upconv1(xe52))
+        print(xe52.shape, xu1.shape, xe42.shape)
+        xu11 = torch.cat([xu1, xe42], dim=1)
+        xd11 = F.relu(self.bn_d11(self.d11(xu11)))
+        xd12 = F.relu(self.bn_d12(self.d12(xd11)))
 
-#         return output
+        xu2 = self.conv1_2(self.upconv2(xd12))
+        xu22 = torch.cat([xu2, xe32], dim=1)
+        xd21 = F.relu(self.bn_d21(self.d21(xu22)))
+        xd22 = F.relu(self.bn_d22(self.d22(xd21)))
+
+        xu3 = self.conv1_3(self.upconv3(xd22))
+        xu33 = torch.cat([xu3, xe22], dim=1)
+        xd31 = F.relu(self.bn_d31(self.d31(xu33)))
+        xd32 = F.relu(self.bn_d32(self.d32(xd31)))
+
+        xu4 = self.conv1_4(self.upconv4(xd32))
+        xu44 = torch.cat([xu4, xe12], dim=1)
+        xd41 = F.relu(self.bn_d41(self.d41(xu44)))
+        xd42 = F.relu(self.bn_d42(self.d42(xd41)))
+
+        # Output layer
+        out = self.outconv(xd42)
+        out = self.sigmoid(out)
+
+        return out
 
 class UNet(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         
         # Encoder
-        # In the encoder, convolutional layers with the Conv2d function are used to extract features from the input image. 
-        # Each block in the encoder consists of two convolutional layers followed by a max-pooling layer, with the exception of the last block which does not include a max-pooling layer.
-        # -------
-        # input: 572x572x3
         self.e11 = nn.Conv2d(3, 64, kernel_size=3, padding=1) # output: 570x570x64
+        self.bn12 = nn.BatchNorm2d(64)
         self.e12 = nn.Conv2d(64, 64, kernel_size=3, padding=1) # output: 568x568x64
+        self.bn12 = nn.BatchNorm2d(64)
+
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 284x284x64
 
         # input: 284x284x64
         self.e21 = nn.Conv2d(64, 128, kernel_size=3, padding=1) # output: 282x282x128
+        self.bn21 = nn.BatchNorm2d(128)
         self.e22 = nn.Conv2d(128, 128, kernel_size=3, padding=1) # output: 280x280x128
+        self.bn22 = nn.BatchNorm2d(128)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 140x140x128
 
         # input: 140x140x128
         self.e31 = nn.Conv2d(128, 256, kernel_size=3, padding=1) # output: 138x138x256
+        self.bn31 = nn.BatchNorm2d(256)
         self.e32 = nn.Conv2d(256, 256, kernel_size=3, padding=1) # output: 136x136x256
+        self.bn32 = nn.BatchNorm2d(256)
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 68x68x256
 
         # input: 68x68x256
         self.e41 = nn.Conv2d(256, 512, kernel_size=3, padding=1) # output: 66x66x512
+        self.bn41 = nn.BatchNorm2d(512)
         self.e42 = nn.Conv2d(512, 512, kernel_size=3, padding=1) # output: 64x64x512
+        self.bn42 = nn.BatchNorm2d(512)
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 32x32x512
 
         # input: 32x32x512
         self.e51 = nn.Conv2d(512, 1024, kernel_size=3, padding=1) # output: 30x30x1024
+        self.bn51 = nn.BatchNorm2d(1024)
         self.e52 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1) # output: 28x28x1024
+        self.bn52 = nn.BatchNorm2d(1024)
 
 
         # Decoder
         self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
         self.d11 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
+        self.bn_d11 = nn.BatchNorm2d(512)
         self.d12 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn_d12 = nn.BatchNorm2d(512)
 
         self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
         self.d21 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
+        self.bn_d21 = nn.BatchNorm2d(256)
         self.d22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        # self.bn_d22 = nn.BatchNorm2d(256)
 
         self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
         self.d31 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+        self.bn_d31 = nn.BatchNorm2d(128)
         self.d32 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.bn_d32 = nn.BatchNorm2d(128)
 
         self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
         self.d41 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
+        self.bn_d41 = nn.BatchNorm2d(64)
         self.d42 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.bn_d42 = nn.BatchNorm2d(64)
 
         # Output layer
         self.outconv = nn.Conv2d(64, num_classes, kernel_size=1)
@@ -222,17 +262,20 @@ class UNet(nn.Module):
         
         # Decoder
         xu1 = self.upconv1(xe52)
+        print('aaaaaaaaaaaaaaaaaaaaaaaaa')
+        print(xu1.shape, xe42.shape)
+        
         xu11 = torch.cat([xu1, xe42], dim=1)
         xd11 = F.relu(self.d11(xu11))
         xd12 = F.relu(self.d12(xd11))
 
         xu2 = self.upconv2(xd12)
-        xu22 = F.torch.cat([xu2, xe32], dim=1)
+        xu22 = torch.cat([xu2, xe32], dim=1)
         xd21 = F.relu(self.d21(xu22))
         xd22 = F.relu(self.d22(xd21))
 
         xu3 = self.upconv3(xd22)
-        xu33 = F.torch.cat([xu3, xe22], dim=1)
+        xu33 = torch.cat([xu3, xe22], dim=1)
         xd31 = F.relu(self.d31(xu33))
         xd32 = F.relu(self.d32(xd31))
 
@@ -258,6 +301,7 @@ if __name__ == '__main__':
     model = PlaxModel(num_classes=6)
     x = torch.randn(2, 3, 256, 256)
     model.train()
+    print(summary(model))
     print(model(x).shape, model(x).min(), model(x).max())
     pred = model(x).detach().numpy()
     # print max and min values
@@ -273,12 +317,23 @@ if __name__ == '__main__':
     model = UNet(num_classes=6)
     model.train()
     x = torch.randn(2, 3, 256, 256)
+    print(summary(model))
     print(model(x).shape, model(x).min(), model(x).max())
     pred = model(x).detach().numpy()
 
     plt.figure()
     plt.imshow(pred[0, 0, :, :])
+    # plt.show()
+
+
+    model = UNet_up(num_classes=6)
+    print(summary(model))
+    x = torch.randn(2, 3, 256, 256)
+    pred = model(x).detach().numpy()
+    plt.figure(num='Un_up')
+    plt.imshow(pred[0, 0, :, :])
     plt.show()
+
 
     
 
