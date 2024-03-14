1d30517ba7dac3091a11e51a511032fcd7ba26a9
AngeloLasala <lasala.angelo96@gmail.com>
2024-03-14 12:24:34 +0100

    add utils file

---
+++ echocardiography/regression/utils.py
@@ -0,0 +1,100 @@
+"""
+This file contains the utility functions for the regression model
+"""
+import numpy as np
+import torch
+
+
+def get_corrdinate_from_heatmap(heatmap):
+    """
+    Get the coordinate from the heatmap
+
+    Parameters
+    ----------
+    heatmap : torch.Tensor
+        Tensor containing the heatmap
+
+    Returns
+    -------
+    list
+        List containing the coordinates
+    """
+    label_list = []
+    for ch in range(heatmap.shape[0]):
+        max_value = np.max(heatmap[ch])
+        coor = np.where(heatmap[ch] == max_value)
+        label_list.append(coor[1][0])
+        label_list.append(coor[0][0])
+    return label_list
+
+def get_corrdinate_from_heatmap_torch(heatmap):
+    """
+    Get the coordinate from the heatmap
+
+    Parameters
+    ----------
+    heatmap : torch.Tensor
+        Tensor containing the heatmap
+
+    Returns
+    -------
+    list
+        List containing the coordinates
+    """
+def get_coordinate_from_heatmap_torch(heatmap):
+    """
+    Get the coordinate from the heatmap
+
+    Parameters
+    ----------
+    heatmap : torch.Tensor
+        Tensor containing the heatmap (B x C * W * H)
+
+    Returns
+    -------
+    list
+        List containing the coordinates
+    """
+    label_list = []
+    print(heatmap.shape)
+    for ch in range(heatmap.shape[1]):
+        max_value, max_index = torch.max(heatmap[ch].view(-1), dim=0)
+        print(max_value, max_index)
+        coor = torch.nonzero(heatmap[ch] == max_value)
+        label_list.append(coor[0][1].item())
+        label_list.append(coor[0][0].item())
+    return label_list
+
+
+def dataset_iteration(dataloader):
+    """
+    Iterate over the dataset
+
+    Parameters
+    ----------
+    dataloader : torch.utils.data.DataLoader
+        DataLoader object that contains the dataset
+    """
+    for batch_idx, (data, target) in enumerate(dataloader):
+        # Your training code goes here
+        # 'data' contains the input images
+        # 'target' contains the corresponding labels or any other relevant information
+        
+        # Example: Print the shape of the batch
+        print(f'Batch {batch_idx + 1}/{len(training_loader)} - Data Shape: {data.shape}, Target Shape: {target.shape}')
+        image = data.numpy().transpose((0, 2, 3, 1))[0]
+        label = target.numpy()[0]
+        print(np.min(image), np.max(image))
+        
+        plt.figure(figsize=(14,14), num='Example - batch ' + str(batch_idx + 1))
+        plt.imshow(image, cmap='gray')
+        plt.scatter(label[0] * image.shape[0], label[1] * image.shape[1], color='green', marker='o', s=100, alpha=0.5) 
+        plt.scatter(label[2] * image.shape[0], label[3] * image.shape[1], color='green', marker='o', s=100, alpha=0.5)
+
+        plt.scatter(label[4] * image.shape[0], label[5] * image.shape[1], color='red', marker='o', s=100, alpha=0.5) 
+        plt.scatter(label[6] * image.shape[0], label[7] * image.shape[1], color='red', marker='o', s=100, alpha=0.5)
+
+        plt.scatter(label[8] * image.shape[0], label[9] * image.shape[1], color='blue', marker='o', s=100, alpha=0.5) 
+        plt.scatter(label[10] * image.shape[0], label[11] * image.shape[1], color='blue', marker='o', s=100, alpha=0.5)
+        plt.axis('off')
+        plt.show()
