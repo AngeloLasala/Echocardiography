6e98dd12181a935e546c71914c08a5a24559c0b7
AngeloLasala <lasala.angelo96@gmail.com>
2024-06-05 16:21:17 +0200

    multiple sampling

---
+++ echocardiography/diffusion/tools/multi_sample.sh
@@ -0,0 +1,8 @@
+python sample_cond_ldm.py --data eco_class_cond --trial trial_7 --experiment cond_ldm_1 --epoch 39
+python sample_cond_ldm.py --data eco_class_cond --trial trial_7 --experiment cond_ldm_1 --epoch 59
+python sample_cond_ldm.py --data eco_class_cond --trial trial_7 --experiment cond_ldm_1 --epoch 79
+python sample_cond_ldm.py --data eco_image_cond --trial trial_7 --experiment cond_ldm_2 --epoch 19
+python sample_cond_ldm.py --data eco_image_cond --trial trial_7 --experiment cond_ldm_2 --epoch 39
+python sample_cond_ldm.py --data eco_image_cond --trial trial_7 --experiment cond_ldm_2 --epoch 59
+python sample_cond_ldm.py --data eco_image_cond --trial trial_7 --experiment cond_ldm_2 --epoch 79
+python sample_cond_ldm.py --data eco_image_cond --trial trial_7 --experiment cond_ldm_2 --epoch 99
