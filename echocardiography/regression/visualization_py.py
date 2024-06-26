7d948130a76c6c3ec5c7708c16f27eeba188b6b3
AngeloLasala <lasala.angelo96@gmail.com>
2024-06-26 11:19:35 +0200

    update visualizzation

--- echocardiography/regression/visualization_py.py
+++ echocardiography/regression/visualization_py.py
@@ -9,6 +9,121 @@ import cv2
 import pandas as pd
 import matplotlib.pyplot as plt
 import tqdm
+from scipy.stats import shapiro
+
+def get_metrics(patients_list, resize):
+    """
+    Compute relevant metric for the hypertrophy dataset
+
+    Parameters:
+    -----------
+    patients_list: list
+        list of patients
+    resize: tuple
+        tuple of the new resolution, preserve aspect ratio (240, 320) -> H/W = 0.75 (median value of the dataset)
+    """
+    rwt_list = []                    ## RWT = 2 * PWT / LVIDd
+    lv_mass_list = []                ## LVM = 0.8 * (1.04 * ((LVIDd + IVSd + PWT) ** 3 - LVIDd ** 3)) + 0.6
+    relative_distance_list = []      ## Relative distance = 2 * PWT / LVIDd (computed on the distances not to measure)
+    rwt_error_list = []              ## RWT error 
+    rst_error_list = []              ## RST error 
+    hypertrofy_list = []     
+    cm_px_list, cm_px_256_list, aspect_ratio_list = [], [], []
+    
+    for patient in tqdm.tqdm(patients_list):
+        patient_label = label[label['HashedFileName'] == patient]        
+        ivsd = patient_label[patient_label['Calc'] == 'IVSd']['CalcValue'].values
+        pwd = patient_label[patient_label['Calc'] == 'LVPWd']['CalcValue'].values
+        lvidd = patient_label[patient_label['Calc'] == 'LVIDd']['CalcValue'].values
+        resolution = patient_label[patient_label['Calc'] == 'Resolution']['CalcValue'].values
+        
+        if len(ivsd) == 0 or len(pwd) == 0 or len(lvidd) == 0:
+            pass
+        
+        else:
+            resolution = (float(patient_label['Height'].values[0]), float(patient_label['Width'].values[0]))
+            aspect_ratio = resolution[0] / resolution[1]  ## H/W
+            ivsd = ivsd[0]
+            pwd = pwd[0]
+            lvidd = lvidd[0]
+
+            ## PW
+            pw_x1 = patient_label[patient_label['Calc'] == 'LVPWd']['X1'].values[0]
+            pw_y1 = patient_label[patient_label['Calc'] == 'LVPWd']['Y1'].values[0]
+            pw_x2 = patient_label[patient_label['Calc'] == 'LVPWd']['X2'].values[0]
+            pw_y2 = patient_label[patient_label['Calc'] == 'LVPWd']['Y2'].values[0]
+
+            pw_distance = np.sqrt((pw_x2 - pw_x1) ** 2 + (pw_y2 - pw_y1) ** 2)
+            pw_distance_256 = 256 * np.sqrt((1/resolution[1]**2)*(pw_x2 - pw_x1) ** 2 + (1 / resolution[0]**2)*(pw_y2 - pw_y1) ** 2) 
+            pw_distance_ar = np.sqrt(((resize[1]/resolution[1])*(pw_x2 - pw_x1)) ** 2 + ((resize[0]/resolution[0])*(pw_y2 - pw_y1)) ** 2) 
+
+            ## LVID
+            lvid_x1 = patient_label[patient_label['Calc'] == 'LVIDd']['X1'].values[0]
+            lvid_y1 = patient_label[patient_label['Calc'] == 'LVIDd']['Y1'].values[0]
+            lvid_x2 = patient_label[patient_label['Calc'] == 'LVIDd']['X2'].values[0]
+            lvid_y2 = patient_label[patient_label['Calc'] == 'LVIDd']['Y2'].values[0]
+
+            lvid_distance = np.sqrt((lvid_x2 - lvid_x1) ** 2 + (lvid_y2 - lvid_y1) ** 2)
+            lvid_distance_256 = 256 * np.sqrt((1/resolution[1]**2)*(lvid_x2 - lvid_x1) ** 2 + (1 / resolution[0]**2)*(lvid_y2 - lvid_y1) ** 2)
+            lvid_distance_ar = np.sqrt(((resize[1]/resolution[1])*(lvid_x2 - lvid_x1)) ** 2 + ((resize[0]/resolution[0])*(lvid_y2 - lvid_y1)) ** 2)
+
+            ## IVSD
+            ivsd_x1 = patient_label[patient_label['Calc'] == 'IVSd']['X1'].values[0]
+            ivsd_y1 = patient_label[patient_label['Calc'] == 'IVSd']['Y1'].values[0]
+            ivsd_x2 = patient_label[patient_label['Calc'] == 'IVSd']['X2'].values[0]
+            ivsd_y2 = patient_label[patient_label['Calc'] == 'IVSd']['Y2'].values[0]
+
+            ivsd_distance = np.sqrt((ivsd_x2 - ivsd_x1) ** 2 + (ivsd_y2 - ivsd_y1) ** 2)
+            ivsd_distance_256 = 256 * np.sqrt((1/resolution[1]**2)*(ivsd_x2 - ivsd_x1) ** 2 + (1 / resolution[0]**2)*(ivsd_y2 - ivsd_y1) ** 2)
+            ivsd_distance_ar = np.sqrt(((resize[1]/resolution[1])*(ivsd_x2 - ivsd_x1)) ** 2 + ((resize[0]/resolution[0])*(ivsd_y2 - ivsd_y1)) ** 2)
+
+            # print(f'PW: {pw_distance:.4f}')
+            # print(f'LVID: {lvid_distance:.4f}')
+            # print(f'IVSD: {ivsd_distance:.4f}')
+            # print(f'cm/pixel (original): {pwd/pw_distance:.4f} - {lvidd/lvid_distance:.4f} - {ivsd/ivsd_distance:.4f}')
+            # print(f'cm/pixel (256): {pwd/pw_distance_256:.4f} - {lvidd/lvid_distance_256:.4f} - {ivsd/ivsd_distance_256:.4f}')
+            # print(f'cm/pixel (aspect ratio): {pwd/pw_distance_ar:.4f} - {lvidd/lvid_distance_ar:.4f} - {ivsd/ivsd_distance_ar:.4f}')
+            cm_px_list.append([lvidd/lvid_distance])
+            cm_px_256_list.append([lvidd/lvid_distance_256])
+
+            ## Compute the RWT
+            rwt = 2 * pwd / lvidd                                   ## RWT compute on dimention (cm)
+            relative_distance = 2 * pw_distance / lvid_distance     ## RWT compute on distance (pixel - original resolution)
+            rwt_256 = 2 * pw_distance_256 / lvid_distance_256       ## RWT compute on distance (pixel - 256 resolution)
+            rwt_ar = 2 * pw_distance_ar / lvid_distance_ar          ## RWT compute on distance (pixel - aspect ratio)
+
+            ## Compute the RST
+            rst = 2 * ivsd / lvidd                                  ## RST compute on dimention (cm)
+            rst_distance = 2 * ivsd_distance / lvid_distance        ## RST compute on distance (pixel - original resolution)
+            rst_256 = 2 * ivsd_distance_256 / lvid_distance_256     ## RST compute on distance (pixel - 256 resolution)
+            rst_ar = 2 * ivsd_distance_ar / lvid_distance_ar        ## RST compute on distance (pixel - aspect ratio)
+
+            lv_mass = 0.8 * (1.04 * ((lvidd + ivsd + pwd) ** 3 - lvidd ** 3)) + 0.6
+            # print(f'rwt: {rwt}, lv_mass: {lv_mass}')
+            # print('RWT')
+            # print(f'rws_distance: {np.abs(rwt - relative_distance)}')
+            # print(f'rwt_256: {np.abs(rwt - rwt_256)}')
+            # print(f'rwt_ar: {np.abs(rwt - rwt_ar)}')
+            # print()
+            # print('RST')
+            # print(f'rst_distance: {np.abs(rst - rst_distance)}')
+            # print(f'rst_256: {np.abs(rst - rst_256)}')
+            # print(f'rst_ar: {np.abs(rst - rst_ar)}')
+            rwt_list.append(rwt)
+            lv_mass_list.append(lv_mass)
+            relative_distance_list.append(relative_distance)
+            aspect_ratio_list.append(aspect_ratio)
+            hypertrofy_list.append([lv_mass, rwt])
+            rwt_error_list.append([rwt - relative_distance, rwt - rwt_256, rwt - rwt_ar])
+            rst_error_list.append([rst - rst_distance, rst - rst_256, rst - rst_ar])
+        # print('==============================================') 
+    
+    return rwt_list, hypertrofy_list, cm_px_list, cm_px_256_list, aspect_ratio_list, rwt_error_list, rst_error_list
+
+if __name__ == '__main__':   
+    parser = argparse.ArgumentParser(description='Visualize the hypertrophy dataset')
+    parser.add_argument('--compute_metrics', action='store_true', help="compute the metrics accross all the data, otherwise load the precomputed one, default=False")
+    args = parser.parse_args()
 
 dataset_dir = "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/Desktop/Phd notes/Echocardiografy/EchoNet-LVH"
 print(os.listdir(dataset_dir))
@@ -19,124 +134,40 @@ label_dir = os.path.join(dataset_dir, 'MeasurementsList.csv')
 label = pd.read_csv(label_dir, index_col=0)
 
 patients = label['HashedFileName'].unique()
-rwt_list = []
-lv_mass_list = []
-relative_distance_list = []
-hypertrofy_list = []
-cm_px_list, cm_px_256_list, aspect_ratio_list = [], [], []
-Save_list = True
-if Save_list:
-    for patient in tqdm.tqdm(patients):
-        patient_label = label[label['HashedFileName'] == patient]        
-        ivsd = patient_label[patient_label['Calc'] == 'IVSd']['CalcValue'].values
-        pwd = patient_label[patient_label['Calc'] == 'LVPWd']['CalcValue'].values
-        lvidd = patient_label[patient_label['Calc'] == 'LVIDd']['CalcValue'].values
-        resolution = patient_label[patient_label['Calc'] == 'Resolution']['CalcValue'].values
-        
-        if len(ivsd) == 0 or len(pwd) == 0 or len(lvidd) == 0:
-            pass
-            # print(len(ivsd), len(pwd), len(lvidd))
-            # print(f'Patient: {patient} has no measurements')
-        
-        else:
-            resolution = (float(patient_label['Width'].values[0]), float(patient_label['Height'].values[0]))
-            aspect_ratio = resolution[1] / resolution[0]
-            # print(resolution[0], resolution[1])
-            ivsd = ivsd[0]
-            pwd = pwd[0]
-            lvidd = lvidd[0]
 
-            pw_x1 = patient_label[patient_label['Calc'] == 'LVPWd']['X1'].values[0]
-            pw_y1 = patient_label[patient_label['Calc'] == 'LVPWd']['Y1'].values[0]
-            pw_x2 = patient_label[patient_label['Calc'] == 'LVPWd']['X2'].values[0]
-            pw_y2 = patient_label[patient_label['Calc'] == 'LVPWd']['Y2'].values[0]
+if args.compute_metrics:
 
-            pw_x1_256 = (pw_x1 / resolution[0]) * 256
-            pw_y1_256 = (pw_y1 / resolution[1]) * 256
-            pw_x2_256 = (pw_x2 / resolution[0]) * 256
-            pw_y2_256 = (pw_y2 / resolution[1]) * 256
-
-            pw_distance = np.sqrt((pw_x2 - pw_x1) ** 2 + (pw_y2 - pw_y1) ** 2)
-            # pw_distance_256 = np.sqrt((pw_x2_256 - pw_x1_256) ** 2 + (pw_y2_256 - pw_y1_256) ** 2)
-            pw_distance_256 = 256 * np.sqrt((1/resolution[0]**2)*(pw_x2 - pw_x1) ** 2 + (1 / resolution[1]**2)*(pw_y2 - pw_y1) ** 2) 
-            pw_distance_ar = np.sqrt(((256/resolution[0])*(pw_x2 - pw_x1)) ** 2 + ((192/resolution[1])*(pw_y2 - pw_y1)) ** 2) 
-
-            lvid_x1 = patient_label[patient_label['Calc'] == 'LVIDd']['X1'].values[0]
-            lvid_y1 = patient_label[patient_label['Calc'] == 'LVIDd']['Y1'].values[0]
-            lvid_x2 = patient_label[patient_label['Calc'] == 'LVIDd']['X2'].values[0]
-            lvid_y2 = patient_label[patient_label['Calc'] == 'LVIDd']['Y2'].values[0]
-
-            lvid_x1_256 = (lvid_x1 / resolution[0]) * 256
-            lvid_y1_256 = (lvid_y1 / resolution[1]) * 256
-            lvid_x2_256 = (lvid_x2 / resolution[0]) * 256
-            lvid_y2_256 = (lvid_y2 / resolution[1]) * 256
-
-            lvid_distance = np.sqrt((lvid_x2 - lvid_x1) ** 2 + (lvid_y2 - lvid_y1) ** 2)
-            # lvid_distance_256 = np.sqrt((lvid_x2_256 - lvid_x1_256) ** 2 + (lvid_y2_256 - lvid_y1_256) ** 2)
-            lvid_distance_256 = 256 * np.sqrt((1/resolution[0]**2)*(lvid_x2 - lvid_x1) ** 2 + (1 / resolution[1]**2)*(lvid_y2 - lvid_y1) ** 2)
-            lvid_distance_ar = np.sqrt(((256/resolution[0])*(lvid_x2 - lvid_x1)) ** 2 + ((192/resolution[1])*(lvid_y2 - lvid_y1)) ** 2)
-
-
-            ivsd_x1 = patient_label[patient_label['Calc'] == 'IVSd']['X1'].values[0]
-            ivsd_y1 = patient_label[patient_label['Calc'] == 'IVSd']['Y1'].values[0]
-            ivsd_x2 = patient_label[patient_label['Calc'] == 'IVSd']['X2'].values[0]
-            ivsd_y2 = patient_label[patient_label['Calc'] == 'IVSd']['Y2'].values[0]
-
-            ivsd_x1_256 = (ivsd_x1 / resolution[0]) * 256
-            ivsd_y1_256 = (ivsd_y1 / resolution[1]) * 256
-            ivsd_x2_256 = (ivsd_x2 / resolution[0]) * 256
-            ivsd_y2_256 = (ivsd_y2 / resolution[1]) * 256
-
-
-            ivsd_distance = np.sqrt((ivsd_x2 - ivsd_x1) ** 2 + (ivsd_y2 - ivsd_y1) ** 2)
-            # ivsd_distance_256 = np.sqrt((ivsd_x2_256 - ivsd_x1_256) ** 2 + (ivsd_y2_256 - ivsd_y1_256) ** 2)
-            ivsd_distance_256 = 256 * np.sqrt((1/resolution[0]**2)*(ivsd_x2 - ivsd_x1) ** 2 + (1 / resolution[1]**2)*(ivsd_y2 - ivsd_y1) ** 2)
-            ivsd_distance_ar = np.sqrt(((256/resolution[0])*(ivsd_x2 - ivsd_x1)) ** 2 + ((192/resolution[1])*(ivsd_y2 - ivsd_y1)) ** 2)
-
-            # print(f'PW: {pw_distance:.4f}')
-            # print(f'LVID: {lvid_distance:.4f}')
-            # print(f'IVSD: {ivsd_distance:.4f}')
-            print(f'cm/pixel (original): {pwd/pw_distance:.4f} - {lvidd/lvid_distance:.4f} - {ivsd/ivsd_distance:.4f}')
-            print(f'cm/pixel (256): {pwd/pw_distance_256:.4f} - {lvidd/lvid_distance_256:.4f} - {ivsd/ivsd_distance_256:.4f}')
-            print(f'cm/pixel (aspect ratio): {pwd/pw_distance_ar:.4f} - {lvidd/lvid_distance_ar:.4f} - {ivsd/ivsd_distance_ar:.4f}')
-            cm_px_list.append([lvidd/lvid_distance])
-            cm_px_256_list.append([lvidd/lvid_distance_256])
-
-            relative_distance = 2 * pw_distance / lvid_distance
-            
-            rwt = 2 * pwd / lvidd
-            rwt_256 = 2 * pw_distance_256 / lvid_distance_256
-            rwt_ar = 2 * pw_distance_ar / lvid_distance_ar
-            lv_mass = 0.8 * (1.04 * ((lvidd + ivsd + pwd) ** 3 - lvidd ** 3)) + 0.6
-            print()
-            print(f'rwt: {rwt}, lv_mass: {lv_mass}')
-            print(f'relative distance: {relative_distance}')
-            print(f'rwt_256: {rwt_256}')
-            print(f'rwt_ar: {rwt_ar}')
-            print(f'pw: {pw_distance}, lvid: {lvid_distance}')
-            rwt_list.append(rwt)
-            lv_mass_list.append(lv_mass)
-            relative_distance_list.append(relative_distance)
-            aspect_ratio_list.append(aspect_ratio)
-            hypertrofy_list.append([lv_mass, rwt])
-        print('==============================================') 
-
+    rwt_list, hypertrofy_list, cm_px_list, cm_px_256_list, aspect_ratio_list, rwt_error_list, rst_error_list = get_metrics(patients, (240, 320))  
 
     hypertrofy_list = np.array(hypertrofy_list)
     cm_px_list = np.array(cm_px_list)
     cm_px_256_list = np.array(cm_px_256_list)
-    # save this list
-    np.save('hypertrofy_list.npy', hypertrofy_list)
-    np.save('cm_px_list.npy', cm_px_list)
-    np.save('cm_px_256_list.npy', cm_px_256_list)
-    np.save('aspect_ratio_list.npy', aspect_ratio_list)
+    aspect_ratio_list = np.array(aspect_ratio_list)
+    rwt_error_list = np.array(rwt_error_list)
+    rst_error_list = np.array(rst_error_list)
+
+    save_folder = 'dataset_metrics'
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+    else:
+        pass
+    np.save(os.path.join(save_folder,'hypertrofy_list.npy'), hypertrofy_list)
+    np.save(os.path.join(save_folder,'cm_px_list.npy'), cm_px_list)
+    np.save(os.path.join(save_folder,'cm_px_256_list.npy'), cm_px_256_list)
+    np.save(os.path.join(save_folder,'aspect_ratio_list.npy'), aspect_ratio_list)
+    np.save(os.path.join(save_folder,'rwt_error_list.npy'), rwt_error_list)
+    np.save(os.path.join(save_folder,'rst_error_list.npy'), rst_error_list)
 
 else:
-    hypertrofy_list = np.load('hypertrofy_list.npy')    
-    cm_px_list = np.load('cm_px_list.npy')
-    cm_px_256_list = np.load('cm_px_256_list.npy')
-    aspect_ratio_list = np.load('aspect_ratio_list.npy')
-    print(np.mean(aspect_ratio_list), np.median(aspect_ratio_list), np.std(aspect_ratio_list))
+    save_folder = 'dataset_metrics'
+    hypertrofy_list = np.load(os.path.join(save_folder,'hypertrofy_list.npy'))
+    cm_px_list = np.load(os.path.join(save_folder,'cm_px_list.npy'))
+    cm_px_256_list = np.load(os.path.join(save_folder,'cm_px_256_list.npy'))
+    aspect_ratio_list = np.load(os.path.join(save_folder,'aspect_ratio_list.npy'))
+    rwt_error_list = np.load(os.path.join(save_folder,'rwt_error_list.npy'))
+    rst_error_list = np.load(os.path.join(save_folder,'rst_error_list.npy'))
+    print(rwt_error_list.shape)
+    print(rst_error_list.shape)
 
 ## 2D scatter plots #####################################################################################################
 
@@ -174,7 +205,6 @@ ax.tick_params(axis='y', labelsize=18)
 
 ## plot the histogram of the cm/px
 fig, ax = plt.subplots(figsize=(8, 8))
-from scipy.stats import shapiro
 stat, p = shapiro(cm_px_list)
 print(f'Shapiro-Wilk Test (origin): p-value: {p}')
 stat, p = shapiro(cm_px_256_list)
@@ -195,6 +225,58 @@ ax.set_ylabel('Number of patient', fontsize=22)
 ax.tick_params(axis='x', labelsize=18)
 ax.tick_params(axis='y', labelsize=18)
 ax.legend(fontsize=18)
+
+## plot the error of the RWT
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(20, 7), num='RWT error', tight_layout=True)
+ax[0].set_title('Original resolution', fontsize=22)
+ax[0].hist(rwt_error_list[:, 0], bins=30, color='gray', alpha=0.7, label= f'mean: {np.mean(rwt_error_list[:, 0]):.4f} - std: {np.std(rwt_error_list[:, 0]):.4f}')
+ax[0].set_xlabel('Error', fontsize=22)
+ax[0].set_ylabel('Number of patient', fontsize=22)
+ax[0].tick_params(axis='x', labelsize=18)
+ax[0].tick_params(axis='y', labelsize=18)
+ax[0].legend(fontsize=18)
+
+ax[1].set_title('256 resolution', fontsize=22)
+ax[1].hist(rwt_error_list[:, 1], bins=30, color='blue', alpha=0.7, label= f'mean: {np.mean(rwt_error_list[:, 1]):.4f} - std: {np.std(rwt_error_list[:, 1]):.4f}')
+ax[1].set_xlabel('Error', fontsize=22)
+ax[1].set_ylabel('Number of patient', fontsize=22)
+ax[1].tick_params(axis='x', labelsize=18)
+ax[1].tick_params(axis='y', labelsize=18)
+ax[1].legend(fontsize=18)
+
+ax[2].set_title('Aspect ratio resolution', fontsize=22)
+ax[2].hist(rwt_error_list[:, 2], bins=30, color='red', alpha=0.7, label= f'mean: {np.mean(rwt_error_list[:, 2]):.4f} - std: {np.std(rwt_error_list[:, 2]):.4f}')
+ax[2].set_xlabel('Error', fontsize=22)
+ax[2].set_ylabel('Number of patient', fontsize=22)
+ax[2].tick_params(axis='x', labelsize=18)
+ax[2].tick_params(axis='y', labelsize=18)
+ax[2].legend(fontsize=18)
+
+## plot the error of the RST
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(20, 7), num='RST error', tight_layout=True)
+ax[0].set_title('Original resolution', fontsize=22)
+ax[0].hist(rst_error_list[:, 0], bins=30, color='gray', alpha=0.7, label= f'mean:{np.mean(rst_error_list[:, 0]):.4f}-std:{np.std(rst_error_list[:, 0]):.4f}')
+ax[0].set_xlabel('Error', fontsize=22)
+ax[0].set_ylabel('Number of patient', fontsize=22)
+ax[0].tick_params(axis='x', labelsize=18)
+ax[0].tick_params(axis='y', labelsize=18)
+ax[0].legend(fontsize=18)
+
+ax[1].set_title('256 resolution', fontsize=22)
+ax[1].hist(rst_error_list[:, 1], bins=30, color='blue', alpha=0.7, label= f'mean:{np.mean(rst_error_list[:, 1]):.4f}-std: {np.std(rst_error_list[:, 1]):.4f}')
+ax[1].set_xlabel('Error', fontsize=22)
+ax[1].set_ylabel('Number of patient', fontsize=22)
+ax[1].tick_params(axis='x', labelsize=18)
+ax[1].tick_params(axis='y', labelsize=18)
+ax[1].legend(fontsize=18)
+
+ax[2].set_title('Aspect ratio resolution', fontsize=22)
+ax[2].hist(rst_error_list[:, 2], bins=30, color='red', alpha=0.7, label= f'mean:{np.mean(rst_error_list[:, 2]):.4f}-std:{np.std(rst_error_list[:, 2]):.4f}')
+ax[2].set_xlabel('Error', fontsize=22)
+ax[2].set_ylabel('Number of patient', fontsize=22)
+ax[2].tick_params(axis='x', labelsize=18)
+ax[2].tick_params(axis='y', labelsize=18)
+ax[2].legend(fontsize=18)
 
 
 ##  3D histogram plot ####################################################################
