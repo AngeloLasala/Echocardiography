726dc04951a371aca704d5910315a3601f60c015
AngeloLasala <lasala.angelo96@gmail.com>
2024-07-16 09:02:26 +0200

    update gif

--- echocardiography/diffusion/create_gif.py
+++ echocardiography/diffusion/create_gif.py
@@ -11,7 +11,7 @@ def create_gif(image_folder, gif_name):
     images = []
     frames = np.arange(999, 1, -1)
     for im in frames:
-        im_path = os.path.join(image_folder, f'x0_{im}.png')
+        im_path = os.path.join(image_folder, f'noise_{im}.png')
         images.append(imageio.imread(im_path))
     imageio.mimsave(f'{gif_name}.gif', images, duration=0.01)
 
@@ -21,4 +21,8 @@ if __name__ == '__main__':
     parser.add_argument('--gif_name', type=str, help='Name of gif to save')
     args = parser.parse_args()
     
+    
+
+    parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    print(parent)
     create_gif(args.image_folder, args.gif_name)
