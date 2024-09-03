de9242a9beae25d3ed514ebc7c71c90d06e36b38
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-03 11:03:05 +0200

    add first attemp of sbatch file

--- echocardiography/regression/train.sh
+++ echocardiography/regression/train.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
-#SBATCH --time=10:00                 # time limits: 1 hour
+#SBATCH --time=15:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
 #SBATCH --error=error_file.err       # standard error file
 #SBATCH --output=file_print.out      # standard output file
@@ -14,7 +14,7 @@ python -m echocardiography.regression.train --data_path "/leonardo_work/IscrC_Me
         --model unet_up \
         --input_channels 1 \
         --size 240 320 \
-        --epochs 5 \
+        --epochs 100 \
         --batch_size=16 \
         --lr 0.001 \
         --weight_decay 0.0 \
