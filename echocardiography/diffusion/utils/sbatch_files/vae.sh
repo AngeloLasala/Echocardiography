3b0872134cedec35dfcc45cace5ddc0bb58db7cf
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-04 15:03:16 +0200

    update the sbatch file

---
+++ echocardiography/diffusion/utils/sbatch_files/vae.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=40:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=vae.err       # standard error file
+#SBATCH --output=vae.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+python -m echocardiography.diffusion.tools.train_vae --data eco_image_cond  --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model'
+ 
