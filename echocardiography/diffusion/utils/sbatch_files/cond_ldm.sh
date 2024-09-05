6197b5f0f7de326ecde0bc3aa3920dd3d76f26ee
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-05 16:56:02 +0200

    updating the file for training ldm on cluster

--- echocardiography/diffusion/utils/sbatch_files/cond_ldm.sh
+++ echocardiography/diffusion/utils/sbatch_files/cond_ldm.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=5:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=vae_all.err       # standard error file
-#SBATCH --output=vae_all.out      # standard output file
+#SBATCH --error=cond_ldm.err       # standard error file
+#SBATCH --output=cond_ldm.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 python -m echocardiography.diffusion.tools.train_cond_ldm --data eco_image_cond\
