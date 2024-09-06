da721459b3da7e863eb427e21946a651c722cc50
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-06 09:05:44 +0200

    updating

--- echocardiography/diffusion/utils/sbatch_files/cond_ldm.sh
+++ echocardiography/diffusion/utils/sbatch_files/cond_ldm.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
-#SBATCH --time=5:00:00                 # time limits: 1 hour
+#SBATCH --time=8:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
 #SBATCH --error=cond_ldm.err       # standard error file
 #SBATCH --output=cond_ldm.out      # standard output file
@@ -11,5 +11,5 @@
 
 python -m echocardiography.diffusion.tools.train_cond_ldm --data eco_image_cond\
           --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/eco'\
-          --trial trial_1
+          --trial trial_2
  
