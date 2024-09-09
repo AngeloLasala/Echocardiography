b04f4bd31931327c86a8c2fffe4054bd76dfddc0
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-09 18:13:31 +0200

    corret sbatch file

--- echocardiography/diffusion/utils/sbatch_files/sample_cond_ldm.sh
+++ echocardiography/diffusion/utils/sbatch_files/sample_cond_ldm.sh
@@ -9,13 +9,13 @@
 #SBATCH --output=sample_cond_ldm.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
-for epoch in 20 40 60 80 100
-    for w in -1.0 0.0 0.2 0.4 0.6 1.0 4.0
-        do
+for epoch in 20 40 60 80 100; do
+    for w in -1.0 0.0 0.2 0.4 0.6 1.0 4.0; do
             python -m echocardiography.diffusion.tools.sample_cond_ldm --data eco_image_cond\
                     --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/'\
                     --trial trial_1\
                     --experiment cond_ldm_1\
                     --epoch $epoch\
                     --guide_w $w
-        done
+     done
+done
