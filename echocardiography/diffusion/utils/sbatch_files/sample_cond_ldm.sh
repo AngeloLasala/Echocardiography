2529c8c3ec950c32e3046b5ad0f51caeb383c2d5
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-09 10:03:08 +0200

    initial import sbatch file for lunck job sample cond ldm

---
+++ echocardiography/diffusion/utils/sbatch_files/sample_cond_ldm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=8:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=cond_ldm.err       # standard error file
+#SBATCH --output=cond_ldm.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+for i in 20 40 60 80 100
+do
+    python -m echocardiography.diffusion.tools.sample_cond_ldm --data eco_image_cond\
+            --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/'\
+            --trial trial_1\
+            --experiment cond_ldm_1\
+            --epoch $i\
+            --guide_w 0.1
+done
