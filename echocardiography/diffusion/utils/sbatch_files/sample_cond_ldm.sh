f4eff566e8a2c8a9cae42cb805035ce9bbad8d74
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-16 17:52:27 +0200

    updating fid and sbatch

--- echocardiography/diffusion/utils/sbatch_files/sample_cond_ldm.sh
+++ echocardiography/diffusion/utils/sbatch_files/sample_cond_ldm.sh
@@ -3,14 +3,14 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
-#SBATCH --time=16:00:00                 # time limits: 1 hour
+#SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=sample_cond_ldm.err       # standard error file
-#SBATCH --output=sample_cond_ldm.out      # standard output file
+#SBATCH --error=sample_cond_ldm_2.err       # standard error file
+#SBATCH --output=sample_cond_ldm_2.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
-for w in 0.0 0.2 0.4; do
-    for epoch in 150; do
+for w in 0.6 0.8 1.0 2.0 4.0 ; do
+    for epoch in 20 40 60 80 100 120; do
             python -m echocardiography.diffusion.tools.sample_cond_ldm --data eco_image_cond_all_batch\
                     --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/'\
                     --trial trial_2\
