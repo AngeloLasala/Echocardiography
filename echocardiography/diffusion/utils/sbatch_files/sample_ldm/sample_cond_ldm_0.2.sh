3b453eb758579e365ccdfe72bcdb23048571480d
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-19 09:51:11 +0200

    update the tree structure of the sbatch file

---
+++ echocardiography/diffusion/utils/sbatch_files/sample_ldm/sample_cond_ldm_0.2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=24:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=sample_cond_ldm_0.2.err       # standard error file
+#SBATCH --output=sample_cond_ldm_0.2.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+for w in 0.2 0.4 ; do
+    for epoch in 20 40 60 80 100 120 150; do
+            python -m echocardiography.diffusion.tools.sample_cond_ldm --data eco_class_cond\
+                    --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/'\
+                    --trial trial_2\
+                    --experiment cond_ldm_2\
+                    --epoch $epoch\
+                    --guide_w $w
+     done
+done
---
+++ echocardiography/diffusion/utils/sbatch_files/train_ddpm/cond_ddpm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --nodes=1                  # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=24:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=cond_ddpm_all.err       # standard error file
+#SBATCH --output=cond_ddpm_all.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+python -m echocardiography.diffusion.tools.train_cond_ddpm --data eco_image_cond_all_batch\
+          --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/eco'\
+          --trial trial_2
+ 
