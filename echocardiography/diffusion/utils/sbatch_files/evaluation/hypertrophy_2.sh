f9213d278cebee8edec6a4f7dae7c0350ed45bf1
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-19 17:09:52 +0200

    add new file

---
+++ echocardiography/diffusion/utils/sbatch_files/evaluation/hypertrophy_2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --nodes=1                  # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=2:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=eval.err       # standard error file
+#SBATCH --output=eval.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+for w in -1.0 0.0 0.2 0.4 0.6 0.8 1.0 2.0; do
+    for epoch in 20 40 60 80 100 120; do
+        python -m echocardiography.diffusion.evaluation.hypertropy_eval\
+                --par_dir '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/eco'\
+                --par_dir_regression '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/regression'\
+                --trial_regression trial_3\
+                --trial trial_2\
+                --experiment cond_ldm_2\
+                --guide_w $w\
+                --epoch $epoch
+    done
+done
+
+ 
