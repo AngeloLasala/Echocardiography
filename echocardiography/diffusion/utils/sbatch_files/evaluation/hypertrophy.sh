f58b55d1a36789fb25b7af5eaf82e863ed7c0b03
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-25 17:43:35 +0200

    sbatch for hypertrophy evaluation

--- echocardiography/diffusion/utils/sbatch_files/evaluation/hypertrophy.sh
+++ echocardiography/diffusion/utils/sbatch_files/evaluation/hypertrophy.sh
@@ -9,14 +9,14 @@
 #SBATCH --output=eval_1.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
-for w in -1.0 0.0 0.2 0.4 0.6 0.8 1.0 2.0; do
+for w in -1.0; do
     for epoch in 20 40 60 80 100 120 150; do
         python -m echocardiography.diffusion.evaluation.hypertropy_eval\
                 --par_dir '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/eco'\
                 --par_dir_regression '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/regression'\
                 --trial_regression trial_3\
                 --trial trial_2\
-                --experiment cond_ldm_1\
+                --experiment ldm_1\
                 --guide_w $w\
                 --epoch $epoch
     done
