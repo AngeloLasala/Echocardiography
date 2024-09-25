c5fadf90acd1029851609636174a2fa01c408eba
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-25 18:06:32 +0200

    updateing sbatch file

--- echocardiography/diffusion/utils/sbatch_files/train_ldm/cond_ldm.sh
+++ echocardiography/diffusion/utils/sbatch_files/train_ldm/cond_ldm.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=18:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=cond_ldm_all.err       # standard error file
-#SBATCH --output=cond_ldm_all.out      # standard output file
+#SBATCH --error=uncond_ldm_all.err       # standard error file
+#SBATCH --output=uncond__ldm_all.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
-python -m echocardiography.diffusion.tools.train_cond_ldm --data eco_class_cond\
+python -m echocardiography.diffusion.tools.train_cond_ldm --data eco_image_cond_all_batch\
           --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/eco'\
           --trial trial_2
  
