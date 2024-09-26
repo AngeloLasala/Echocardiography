3a32b7fc7e6455ba19eeb461a5e2f0e72e3d2d68
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-26 11:03:59 +0200

    refinement of distributed data parell

--- echocardiography/diffusion/utils/dist_utils.py
+++ echocardiography/diffusion/utils/dist_utils.py
@@ -55,9 +55,36 @@ def get_resources(verbose=True):
 
     return rank, local_rank, world_size, local_size, num_workers
 
+# def setup_dist():
+#     """
+#     Setup a distributed process group.
+#     """
+#     if dist.is_initialized():
+#         return
+
+#     comm = MPI.COMM_WORLD
+#     backend = "gloo" if not th.cuda.is_available() else "nccl"
+
+#     if backend == "gloo":
+#         hostname = "localhost"
+#     else:
+#         hostname = socket.gethostbyname(socket.getfqdn())
+#     os.environ["MASTER_ADDR"] = comm.bcast(hostname, root=0)
+#     os.environ["RANK"] = str(comm.rank)
+#     os.environ["WORLD_SIZE"] = str(comm.size)
+
+#     port = comm.bcast(_find_free_port(), root=0)
+#     os.environ["MASTER_PORT"] = str(port)
+#     dist.init_process_group(backend=backend, init_method="env://")
 
 
-# dist.init_process_group("nccl", rank=rank, world_size=world_size)
+# def dev():
+    """
+    Get the device to use for torch.distributed.
+    """
+    if th.cuda.is_available():
+        return th.device(f"cuda:{MPI.COMM_WORLD.Get_rank() % GPUS_PER_NODE}")
+    return th.device("cpu")
 
 
 
@@ -66,6 +93,24 @@ if __name__ == '__main__':
     # Initialize the distributed training environment
     # setup_dist()
     rank, local_rank, world_size, local_size, num_workers = get_resources()
-    print(rank, local_rank, world_size, local_size, num_workers)
 
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+    if rank == 0:
+        print(f"Dataset path .............. {dataset_path}")
+        print(f"Num. Images ............... {len(dataset)}")
+        print(f"Batch Size ................ {batch_size}")
+        print(f"Num. Batches .............. {len(loader)}")
+        print(f"Num. processors ........... {world_size}")
+        print(f"Num. GPUs per node ........ {local_size}")
+        print(f"Num. Workers .............. {num_workers}")
+        print(f"Device ....................", "GPU" if torch.cuda.is_available() else "CPU")
+        print(f"GPU device name ........... {torch.cuda.get_device_name(0)}")
+        print(f"Num. of available GPUs .... {torch.cuda.device_count()}")
+        print(f"Num. of processes ......... {world_size}")
+
+    device = torch.device("cuda:{}".format(local_rank))
+    # each process exclusively works on a single GPU
+    torch.cuda.set_device(local_rank)
+    print(device)
    
