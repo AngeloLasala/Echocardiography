b80613971ff1a723a1f6612df665d63cfcc10563
AngeloLasala <lasala.angelo96@gmail.com>
2024-09-26 11:16:47 +0200

    refinement of distributed data parell

--- echocardiography/diffusion/utils/sbatch_files/train_ddpm/prova_ddp.sh
+++ echocardiography/diffusion/utils/sbatch_files/train_ddpm/prova_ddp.sh
@@ -9,5 +9,8 @@
 #SBATCH --output=ddp.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
+export MASTER_ADDR=$(srun --ntasks=1 hostname 2>&1 | tail -n1)
+export MASTER_PORT=29500
+
 python -m echocardiography.diffusion.utils.dist_utils
  
