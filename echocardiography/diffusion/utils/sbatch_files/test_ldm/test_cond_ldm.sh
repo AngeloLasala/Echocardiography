9cef7805ff01a9cead9cec96cb5209b7e461cc5c
AngeloLasala <lasala.angelo96@gmail.com>
2024-10-01 11:34:34 +0200

    sbatch file for test_ldm

---
+++ echocardiography/diffusion/utils/sbatch_files/test_ldm/test_cond_ldm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=24:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=test_ldm.err       # standard error file
+#SBATCH --output=test_ldm.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+
+python -m echocardiography.diffusion.tools.test_cond_ldm\
+        --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/'\
+        --trial trial_2\
+        --experiment cond_ldm_1\
+        --epoch 100\
+        --guide_w 0.6
