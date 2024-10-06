95a47247cccdff08db985e6ba9ec9db8f1ab7966
AngeloLasala <lasala.angelo96@gmail.com>
2024-10-06 12:26:18 +0200

    update the general version of sample_cond_ldm

--- echocardiography/diffusion/utils/sbatch_files/sample_ldm/sample_cond_ldm.sh
+++ echocardiography/diffusion/utils/sbatch_files/sample_ldm/sample_cond_ldm.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=sample_cond_ldm_2.err       # standard error file
-#SBATCH --output=sample_cond_ldm_2.out      # standard output file
+#SBATCH --error=sample_cond_ldm_3.err       # standard error file
+#SBATCH --output=sample_cond_ldm_3.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 # python -m echocardiography.diffusion.tools.sample_cond_ldm --data eco_image_cond_all_batch\
