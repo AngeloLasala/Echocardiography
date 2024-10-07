d528f952180fc20709983eeb55172989942f01dc
AngeloLasala <lasala.angelo96@gmail.com>
2024-10-07 14:37:00 +0200

    add new sbatch file

---
+++ echocardiography/diffusion/utils/sbatch_files/train_ldm/cond_ldm_2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=18:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=double_cond_ldm_img.err       # standard error file
+#SBATCH --output=double_cond_ldm_img.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+python -m echocardiography.diffusion.tools.train_cond_ldm --data eco_image_cond_all_batch\
+          --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/eco'\
+          --trial trial_4\
+          --cond_ldm
+ 
