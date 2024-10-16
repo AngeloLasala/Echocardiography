ddb495bff40849f936cf0d80a2aff9f734e996f4
AngeloLasala <lasala.angelo96@gmail.com>
2024-10-16 11:30:52 +0200

    important update: modify the condition w.r.t. the linear relationship rst and rwt

--- echocardiography/diffusion/utils/sbatch_files/test_ldm/test_cond_ldm.sh
+++ echocardiography/diffusion/utils/sbatch_files/test_ldm/test_cond_ldm.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=test__ldm.err       # standard error file
-#SBATCH --output=test_ldm.out      # standard output file
+#SBATCH --error=test_linear.err       # standard error file
+#SBATCH --output=test_linear.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 
