9cdec37cf856254db87e3c99576c1cba59f2ef47
AngeloLasala <lasala.angelo96@gmail.com>
2024-10-24 09:18:21 +0200

    update ecopar conditioning

--- echocardiography/diffusion/utils/sbatch_files/sample_ldm/sample_double_cond_4_6.sh
+++ echocardiography/diffusion/utils/sbatch_files/sample_ldm/sample_double_cond_4_6.sh
@@ -5,20 +5,19 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=sample_double_4_6.err       # standard error file
-#SBATCH --output=sample_double_4_6.out      # standard output file
+#SBATCH --error=ecopar_4_6.err       # standard error file
+#SBATCH --output=ecopar_4_6.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
-for trial in trial_5; do
+for trial in trial_2; do
     for w in 0.4 0.6 ; do
         for epoch in 20 40 60 80 100 120 150; do
                 python -m echocardiography.diffusion.tools.sample_cond_ldm\
                         --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/'\
                         --trial $trial\
-                        --experiment cond_ldm_2\
+                        --experiment cond_ldm_5\
                         --epoch $epoch\
                         --guide_w $w\
-                        --cond_ldm
 
          done
     done
