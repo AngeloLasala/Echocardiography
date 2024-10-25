f9efee61c3ff34ca67379e4a67fd8f891f7f252a
AngeloLasala <lasala.angelo96@gmail.com>
2024-10-25 10:24:38 +0200

    update sbatch file

--- echocardiography/diffusion/utils/sbatch_files/evaluation/hypertrophy_2.sh
+++ echocardiography/diffusion/utils/sbatch_files/evaluation/hypertrophy_2.sh
@@ -15,8 +15,8 @@ for w in 0.0 0.2 0.4 0.6 0.8 1.0; do
                 --par_dir '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/eco'\
                 --par_dir_regression '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/regression'\
                 --trial_regression trial_3\
-                --trial trial_5\
-                --experiment cond_ldm_1\
+                --trial trial_2\
+                --experiment cond_ldm_4\
                 --guide_w $w\
                 --epoch $epoch
     done
