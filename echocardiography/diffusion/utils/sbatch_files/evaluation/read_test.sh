17f725d8d1e6f05566841316502be2e98d465415
AngeloLasala <Angelo.Lasala@santannapisa.it>
2024-11-18 14:54:49 +0100

    update the read_test.py file

--- echocardiography/diffusion/utils/sbatch_files/evaluation/read_test.sh
+++ echocardiography/diffusion/utils/sbatch_files/evaluation/read_test.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=2:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=eval_2.err       # standard error file
-#SBATCH --output=eval_2.out      # standard output file
+#SBATCH --error=read_test.err       # standard error file
+#SBATCH --output=read_test.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 python -m echocardiography.diffusion.evaluation.read_test --par_dir "/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/eco"\
