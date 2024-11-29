63a0dd0b35f8f979edf47e2c12ee6881ad4b48e9
AngeloLasala <Angelo.Lasala@santannapisa.it>
2024-11-29 15:11:59 +0100

    update evaluation

--- echocardiography/diffusion/utils/sbatch_files/evaluation/hypertrophy_2.sh
+++ echocardiography/diffusion/utils/sbatch_files/evaluation/hypertrophy_2.sh
@@ -9,7 +9,7 @@
 #SBATCH --output=eval_2.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
-for w in 0.0 0.2 0.4 0.6 0.8 1.0; do
+for w in 0.0 0.2 0.4 0.8; do
     for epoch in 20 40 60 80 100 120 150; do
         python -m echocardiography.diffusion.evaluation.hypertropy_eval\
                 --par_dir '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/eco'\
