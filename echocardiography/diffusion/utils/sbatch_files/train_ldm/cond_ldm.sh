5f002b128bc6966286e512a29493bcf806d62ff9
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-01-22 15:49:28 +0100

    update version for simple heatmaps

--- echocardiography/diffusion/utils/sbatch_files/train_ldm/cond_ldm.sh
+++ echocardiography/diffusion/utils/sbatch_files/train_ldm/cond_ldm.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=18:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=relative_cond_ldm.err       # standard error file
-#SBATCH --output=relative_cond_ldm.out      # standard output file
+#SBATCH --error=simple_heat_cond_ldm.err       # standard error file
+#SBATCH --output=simple_heat_cobd_ldm.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
-python -m echocardiography.diffusion.tools.train_cond_ldm --data eco_class_relative_cond\
+python -m echocardiography.diffusion.tools.train_cond_ldm --data eco_image_simple_cond\
           --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/eco'\
           --trial trial_2
  
