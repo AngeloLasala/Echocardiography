28207a98dd1fe9e7450e41837129a51b86a4eab1
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-01-23 10:02:35 +0100

    update the sbatch file

--- echocardiography/diffusion/utils/sbatch_files/sample_ldm/sample_cond_ldm_4.sh
+++ echocardiography/diffusion/utils/sbatch_files/sample_ldm/sample_cond_ldm_4.sh
@@ -5,12 +5,12 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=simple_2.err       # standard error file
-#SBATCH --output=simple_2.out      # standard output file
+#SBATCH --error=simple_4.err       # standard error file
+#SBATCH --output=simple_4.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 for trial in trial_2; do
-    for w in 0.2; do
+    for w in 0.4; do
         for epoch in 20 40 60 100 120 150; do
                 python -m echocardiography.diffusion.tools.sample_cond_ldm\
                         --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/diffusion/'\
