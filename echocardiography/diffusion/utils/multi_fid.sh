f8665c30f7dfeaebcbcc0dcaab005982d4649658
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-01-24 11:07:42 +0100

    update the multi fid evaluation

--- echocardiography/diffusion/utils/multi_fid.sh
+++ echocardiography/diffusion/utils/multi_fid.sh
@@ -3,6 +3,6 @@ for w in 0.0 0.2 0.4 0.6 0.8; do
     python -m echocardiography.diffusion.evaluation.fid\
     --par_dir "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Echocardiografy/trained_model/diffusion/eco/"\
     --trial trial_2\
-    --experiment cond_ldm_6\
+    --experiment cond_ldm_8\
     --guide_w $w
     done
